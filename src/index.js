28b43720ec9b7fba40c6d1600f5ebea37d140d2f
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2018-05-10 13:07:41 +0800

    Adding Simple Provider working Perfect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './tutorial/App';
+import App from './simple/app'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/tutorial/App.js
+++ src/tutorial/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard';
+import authProvider from './authProvider';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
+//import dataProvider from './dataProvider';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin 
     dataProvider={dataProvider}
+    dashboard={Dashboard}
+    authProvider={authProvider}
     title="Cloud-Based Realtime Heart Rate Monitoring System"
     >
-        <Resource name="posts" list={PostList} />
-        <Resource name="users" list={UserList} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+        <Resource name="users" list={UserList} icon={UserIcon} />
 
     </Admin>
 );
