4b2b4d900810460b7c4ff2e503cab41d77df74c9
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2018-05-12 17:08:37 +0800

    Fix Bugs

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cd /root/heart-rate-system && git reset --hard HEAD && git pull && yarn && cd /root/heart-rate-system/server && npm i && npm install -g nodemon && node app.js && cd /root/heart-rate-system && REACT_APP_DEV_API_URL=https://35.232.234.165:3001 yarn start
+cd /root/heart-rate-system && git reset --hard HEAD && git pull && yarn && cd /root/heart-rate-system/server && npm i && nodemon app.js && cd /root/heart-rate-system && REACT_APP_DEV_API_URL=https://35.232.234.165:3001 yarn start
 EOF
