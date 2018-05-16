8dc98012c91c2ff18dd498ace63ebf66e065db74
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2018-05-16 23:37:35 +0800

    Testing the Functions Working

--- arduino/PulseSensorAmped_Arduino/PulseSensorAmped_Arduino.ino
+++ arduino/PulseSensorAmped_Arduino/PulseSensorAmped_Arduino.ino
@@ -1,35 +1,4 @@
 
-/*
->> Pulse Sensor Amped 1.1 <<
-This code is for Pulse Sensor Amped by Joel Murphy and Yury Gitman
-    www.pulsesensor.com 
-    >>> Pulse Sensor purple wire goes to Analog Pin 0 <<<
-Pulse Sensor sample aquisition and processing happens in the background via Timer 2 interrupt. 2mS sample rate.
-PWM on pins 3 and 11 will not work when using this code, because we are using Timer 2!
-The following variables are automatically updated:
-Signal :    int that holds the analog signal data straight from the sensor. updated every 2mS.
-IBI  :      int that holds the time interval between beats. 2mS resolution.
-BPM  :      int that holds the heart rate value, derived every beat, from averaging previous 10 IBI values.
-QS  :       boolean that is made true whenever Pulse is found and BPM is updated. User must reset.
-Pulse :     boolean that is true when a heartbeat is sensed then false in time with pin13 LED going out.
-
-This code is designed with output serial data to Processing sketch "PulseSensorAmped_Processing-xx"
-The Processing sketch is a simple data visualizer. 
-All the work to find the heartbeat and determine the heartrate happens in the code below.
-Pin 13 LED will blink with heartbeat.
-If you want to use pin 13 for something else, adjust the interrupt handler
-It will also fade an LED on pin fadePin with every beat. Put an LED and series resistor from fadePin to GND.
-Check here for detailed code walkthrough:
-http://pulsesensor.myshopify.com/pages/pulse-sensor-amped-arduino-v1dot1
-
-Code Version 02 by Joel Murphy & Yury Gitman  Fall 2012
-This update changes the HRV variable name to IBI, which stands for Inter-Beat Interval, for clarity.
-Switched the interrupt to Timer2.  500Hz sample rate, 2mS resolution IBI value.
-Fade LED pin moved to pin 5 (use of Timer2 disables PWM on pins 3 & 11).
-Tidied up inefficiencies since the last version. 
-*/
-
-
 //  VARIABLES
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
--- raspberrypi/readserial.py
+++ raspberrypi/readserial.py
@@ -4,7 +4,7 @@ import serial
 import requests
 import json
 import re
-
+from random import randint
 
 ser = serial.Serial(
 #    port='/dev/ttyACM0',
@@ -24,7 +24,12 @@ while 1:
         bpm = re.sub(r'\r\n', '', x)
         milli_sec = int(round(time.time() * 1000))
         url = 'https://smart-heart-rate.firebaseio.com/pulses/' + str(milli_sec) + '/.json'
-        payload = {'key1': 'value1', 'bpm': bpm}
+        payload = {
+            'id': 0,
+            'time': int(milli_sec),
+            'uid': randint(1, 3),
+            'bpm': int(bpm)
+            }
         r = requests.put(url, data=json.dumps(payload))
         print r.text
         print r.status_code
