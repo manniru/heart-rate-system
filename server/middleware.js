9a52e4ed66d0c423c84b1ab9052ed1b7890ef00a
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2018-05-16 21:24:41 +0800

    Edit Config file

--- server/middleware.js
+++ server/middleware.js
@@ -1,11 +1,11 @@
 //json-server --watch db.json --middlewares ./middleware.js --port 3001
-var phone = '+601151212963';
+var config = require('./config.js');
 
 module.exports = (req, res, next) => {
     res.header('X-Hello', 'World')
     if (req.body.bpm >=150) {
         var message = "This is SMS Message From Heart Rate Monitoring System. Your Patient with ID="+req.body.uid+" his Heart Rate is  "+req.body.bpm+" is Getting High Please, try to Call him, Thank You";
-        console.log("SMS Message Sent! to Phone:" + phone +' , Message '+ message);
+        console.log("SMS Message Sent! to Phone:" + config.phone +' , Message '+ message);
     }
     next()
   }
